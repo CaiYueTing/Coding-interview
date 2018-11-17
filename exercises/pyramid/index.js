33fe5f38c3789be80dec608332a97fcbe3b850a5
YTC <qazxcv367tasi@gmail.com>
2018-11-17 14:34:23 +0800

    pyramid compelted

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,16 +14,32 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    const totalcol = (n+1)/2
+// function pyramid(n) {
+//     const totalcol = n*2-1
+//     const midpoint = n-1 // Math.floor(totalcol/2)
+//     for (let row =0; row<n ;row++){
+//         let str = ''
+//         for (let col=0; col<totalcol;col++){
+//             (col>=n-1-row && col<=n-1+row)?str += '#':str +=' '            
+//             // (col>=midpoint-row && col<=midpoint+row)?str += '#':str +=' '
+//         }
+//         console.log(str)
+//     }
+// }
 
-    for (let row =0; row<n ;row++){
-        let str = ''
-        for (let col=0; col<totalcol;col++){
-            (row<=col)? str+='#' :' '
-        }
+function pyramid(n, row =0, str ='') {
+    if (row ===n){
+        return;
     }
 
+    if (str.length === 2*n-1){
+        console.log(str)
+        return pyramid(n, row+1)
+    }
+    let col = str.length
+    let add;
+    (col>=n-1-row && col<=n-1+row)?add = '#':add =' '  
+    pyramid(n,row, str+add)
 }
 
 module.exports = pyramid;
