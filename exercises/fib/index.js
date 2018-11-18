11c57bdda0f11d0643bf0899e7cdc8712c22e774
YTC <qazxcv367tasi@gmail.com>
2018-11-18 21:05:11 +0800

    fib add memorize

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,23 +8,39 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    const result = [0,1]
 
-    for (let i=2;i<=n;i++){
-        const a = result[i-1]
-        const b = result[i-2]
-        result.push(a+b)
+function memoize(fn){
+    const cache ={}
+    return function (...args) {
+        if (cache[args]){
+            return cache[args]
+        }
+        const result = fn.apply(this, args)
+        cache[args] = result
+
+        return result
     }
-    console.log(result)
-    return result[n]
 }
+
+function slowfib(n) {
+    if (n==0 || n==1){
+        return n
+    }
+    return fib(n-1)+fib(n-2)
+}
+
+const fib = memoize(slowfib)
 
 module.exports = fib;
 
 // function fib(n) {
-//     if (n==0 || n==1){
-//         return n
+//     const result = [0,1]
+
+//     for (let i=2;i<=n;i++){
+//         const a = result[i-1]
+//         const b = result[i-2]
+//         result.push(a+b)
 //     }
-//     return fib(n-1)+fib(n-2)
+//     console.log(result)
+//     return result[n]
 // }
