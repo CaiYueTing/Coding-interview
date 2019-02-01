92bee742eafc229b1d50b7b070650dfe0cc5b4c5
YTC <qazxcv367tasi@gmail.com>
2019-02-01 10:56:01 +0800

    all complete

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,57 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for(let i=0; i<arr.length; i++){
+        for (let j=0; j<arr.length-1;j++){
+            if (arr[j]>arr[j+1]){
+                const a = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = a               
+            }
+        }
+    }
+    return arr
 }
 
 function selectionSort(arr) {
-
+    for (let i=0;i<arr.length;i++){
+        let indexToMin = i
+        for (let j=i+1;j<arr.length;j++){
+            if (arr[indexToMin]>arr[j]){
+                indexToMin = j
+            }
+        }
+        const a = arr[i]
+        arr[i] = arr[indexToMin]
+        arr[indexToMin] = a
+    }
+    return arr
 }
 
 function mergeSort(arr) {
+    if (arr.length ===1){
+        return arr
+    }
 
+    const center = Math.floor(arr.length / 2)
+    const left = arr.slice(0, center)
+    const right = arr.slice(center)
+
+    return merge(mergeSort(left),mergeSort(right))
 }
 
 function merge(left, right) {
+    const results = []
 
+    while(left.length && right.length){
+        if (left[0]<right[0]){
+            results.push(left.shift())
+        }else {
+            results.push(right.shift())
+        }
+    }
+
+    return [...results,...left,...right]
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
